f132c72ed7c0478fb3fe22382b7f473a1f591c3e
NahuelCattani89 <nahucattani@gmail.com>
2017-06-27 21:34:34 -0300

    llegue hasta el punto 4.

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,13 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
 {
-	
+    alert("esto funciona de maravilla");
+
+
+
+
+
+
+    
 }
 
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,7 @@
 */
 function Mostar()
 {
+	promp
 	
 }
 
